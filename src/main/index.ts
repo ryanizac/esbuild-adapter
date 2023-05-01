0d8c8a9a1b9cdfed67745673263b39a11a831f5e
Ryan Izac <ryanizac.infor@gmail.com>
2023-04-30 21:29:52 -0300

    ref: transform main function in class

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,26 +3,34 @@ import { Options } from "../options";
 import { WatchBuildPlugin } from "../plugins";
 import * as utils from "../utils";
 
-async function main() {
-  const [buildOptions, compilerOptions] = await Options.load();
+class Application {
+  private static async loadOptions() {
+    const options = await Options.load();
+    const buildOptions = options[0];
 
-  if (utils.checkEmptyObject(buildOptions)) {
-    console.log("[compiler] no build options");
-    process.exit(1);
+    if (utils.checkEmptyObject(buildOptions)) {
+      console.log("[compiler] no build options");
+      process.exit(1);
+    }
+
+    return options;
   }
 
-  const esbuild = new Esbuild(buildOptions);
+  static async main() {
+    const [buildOptions, compilerOptions] = await this.loadOptions();
+    const esbuild = new Esbuild(buildOptions);
 
-  esbuild.addPlugin(WatchBuildPlugin);
+    esbuild.addPlugin(WatchBuildPlugin);
 
-  if (!compilerOptions.watch) {
-    await esbuild.pureBuild();
-    console.log("[compiler] Closing compiler");
-    process.exit();
+    if (!compilerOptions.watch) {
+      await esbuild.pureBuild();
+      console.log("[compiler] Closing compiler");
+      process.exit();
+    }
+
+    await esbuild.watchMode();
+    console.log("[compiler] Watching changes");
   }
-
-  await esbuild.watchMode();
-  console.log("[compiler] Watching changes");
 }
 
-main();
+Application.main();
